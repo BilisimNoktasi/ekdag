a82698b13e3090caed36081493b883c115042edb
BilisimNoktasi <info@bilisimnoktasi.com.tr>
2024-12-19 17:45:20 +0300

    son-push

--- components/News/index.tsx
+++ components/News/index.tsx
@@ -1,5 +1,4 @@
 'use client'
-import Image from 'next/image'
 import React, { useEffect, useState } from 'react'
 import { Autoplay } from 'swiper/modules'
 import { Swiper, SwiperSlide } from 'swiper/react'

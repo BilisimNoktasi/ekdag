dc66e625908e55e6417c0fa39da4411c06eede8e
BilisimNoktasi <info@bilisimnoktasi.com.tr>
2025-01-08 15:29:20 +0300

    finalkee

--- app/[locale]/kurumsal/yonetim/muhittin-bocek/page.tsx
+++ app/[locale]/kurumsal/yonetim/muhittin-bocek/page.tsx
@@ -12,8 +12,8 @@ export default function DocsPage() {
   return (
     <>
       <section className="pb-16 pt-24 md:pb-20 md:pt-28 lg:pb-24 lg:pt-32">
-        <div className="container mx-auto">
-            <div className="w-full px-4 lg:w-1/4">
+        <div className="container mx-auto flex md:flex-row flex-col sm:flex-col">
+            <div className="w-full px-4 md:w-1/4">
               <div className="sticky top-[74px] rounded-lg border border-white p-4 shadow-solid-4  transition-all  dark:border-strokedark dark:bg-blacksection">
                 <ul className="space-y-2">
                   <img src="/images/yonetim/muhittin-bocek-01.jpg" />
@@ -21,7 +21,7 @@ export default function DocsPage() {
               </div>
             </div>
 
-            <div className="w-full px-4 lg:w-3/4">
+            <div className="w-full px-4 md:w-3/4 sm:w-full">
               <div className="blog-details blog-details-docs shadow-three dark:bg-gray-dark rounded-sm bg-white px-8 py-11 sm:p-[55px] lg:mb-5 lg:px-8 xl:p-[55px]">
                 <h3>{t("title")}</h3>
                 <h1>Muhittin BÖCEK</h1>
